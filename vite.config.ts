144640a00dfe8324c22034ac5b1d210d938e4386
Tuan <147364431+Gazille@users.noreply.github.com>
2024-08-27 13:50:41 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-react-ts.git/",
+  base: "/vite-react-ts/",
   plugins: [react()],
 })
